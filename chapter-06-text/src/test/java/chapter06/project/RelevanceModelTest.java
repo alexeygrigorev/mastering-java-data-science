7f9fa569860546435a866f214ba8a98d2ef4c371
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2016-12-06 14:34:46 +0100

    renamed the test as well

--- chapter-06-text/src/test/java/chapter06/project/RelevanceModelTest.java
+++ chapter-06-text/src/test/java/chapter06/project/RelevanceModelTest.java
@@ -6,13 +6,13 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
-public class FirstPageOrNotTest {
+public class RelevanceModelTest {
 
     @Test
     public void concat1d() {
         double[] y1 = { 1.0, 2.0, 3.0 };
         double[] y2 = { 4.0, 5.0 };
-        double[] y = FirstPageOrNot.concat(y1, y2);
+        double[] y = RelevanceModel.concat(y1, y2);
         double[] expected = { 1.0, 2.0, 3.0, 4.0, 5.0 };
         assertTrue(Arrays.equals(y, expected));
     }
@@ -21,7 +21,7 @@ public class FirstPageOrNotTest {
     public void concat2d() {
         double[][] X1 = { { 0.0, 1.0 }, { 0.0, 2.0 }, { 0.0, 3.0 } };
         double[][] X2 = { { 0.0, 4.0 }, { 0.0, 5.0 } };
-        double[][] X = FirstPageOrNot.concat(X1, X2);
+        double[][] X = RelevanceModel.concat(X1, X2);
         double[][] expected = { { 0.0, 1.0 }, { 0.0, 2.0 }, { 0.0, 3.0 }, { 0.0, 4.0 }, { 0.0, 5.0 } };
         assertTrue(Arrays.deepEquals(X, expected));
     }
